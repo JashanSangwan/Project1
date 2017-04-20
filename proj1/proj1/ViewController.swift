8f28096474639e186df0b9c21be61ef1a562ae5d
JASHAN SANGWAN <jsangwa@10-150-45-27.dhcp-m.deakin.edu.au>
2017-04-20 11:44:55 +1000

    1st checkin

---
+++ proj1/proj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by JASHAN SANGWAN on 20/04/2017.
+//  Copyright © 2017 JASHAN SANGWAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
